9f37b3cb6d961ef98782500823fdec49cc90b90f
fbo1982 <f.bopp@cae-automation.de>
2026-04-17 18:54:11 +0200

    Delete run.sh

--- run.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/with-contenv bashio
-set -euo pipefail
-
-export PYTHONPATH="/opt/car2mqtt/app"
-export CAR2MQTT_DATA_DIR="/data/car2mqtt"
-mkdir -p "$CAR2MQTT_DATA_DIR"
-
-exec python3 -m car2mqtt.main
