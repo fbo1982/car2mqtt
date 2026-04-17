567e3068aec9c83f1f241c76f546fcd2e38c8caf
fbo1982 <f.bopp@cae-automation.de>
2026-04-17 23:22:57 +0200

    Add files via upload

--- car2mqtt/app/api/server.py
+++ car2mqtt/app/api/server.py
@@ -122,7 +122,7 @@ def create_app() -> FastAPI:
             {
                 "cards": cards,
                 "providers": providers,
-                "version": "0.4.0",
+                "version": "0.4.1",
                 "mqtt_settings": mqtt_settings,
                 "cards_json": json.dumps(cards, ensure_ascii=False),
             },
