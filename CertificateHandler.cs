c6b2e5a8fac620bce846f38525f0fda0e10f4a91
fbo1982 <f.bopp@cae-automation.de>
2026-04-18 02:34:06 +0200

    Delete CertificateHandler.cs

--- CertificateHandler.cs
+++
@@ -1,9 +0,0 @@
-from app.api.server import create_app
-import os
-import uvicorn
-
-app = create_app()
-
-if __name__ == "__main__":
-    port = int(os.getenv("APP_PORT", "8099"))
-    uvicorn.run(app, host="0.0.0.0", port=port)
