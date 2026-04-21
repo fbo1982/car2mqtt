c57a95bdab2ce2bcb1c856c0b519dff22c1d56ab
fbo1982 <f.bopp@cae-automation.de>
2026-04-21 10:33:35 +0200

    V0.9.11

--- car2mqtt/app/api/server.py
+++ car2mqtt/app/api/server.py
@@ -133,7 +133,7 @@ def create_app() -> FastAPI:
             {
                 "cards": cards,
                 "providers": providers,
-                "version": "0.9.10",
+                "version": "0.9.11",
                 "mqtt_settings": mqtt_settings,
                 "cards_json": json.dumps(cards, ensure_ascii=False),
             },
